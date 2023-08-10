9f853bb368eac62f4ee2690c667b46d7e096c620
ash_zac <asherzac2020@gmail.com>
2023-08-10 09:46:00 -0700

    hello-first-message

---
+++ cohere-ai/index.ts
@@ -0,0 +1,55 @@
+import axios, { AxiosResponse } from "axios";
+
+interface Generation {
+  id: string;
+  text: string;
+}
+
+interface CohereResponse {
+  id: string;
+  generations: Generation[];
+  prompt: string;
+  meta: {
+    api_version: {
+      version: string;
+    };
+  };
+}
+
+export class Cohere {
+  private apiKey: string;
+
+  constructor(apiKey: string) {
+    this.apiKey = apiKey;
+  }
+
+  get options() {
+    return {
+      method: "POST",
+      url: "https://api.cohere.ai/v1/generate",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+        authorization: `Bearer ${this.apiKey}`,
+      },
+      data: {
+        max_tokens: 20,
+        return_likelihoods: "NONE",
+        truncate: "END",
+        prompt: "Please explain to me how LLMs work",
+      },
+    };
+  }
+
+  public fetchData() {
+    axios
+      .request<CohereResponse>(this.options)
+      .then((response: AxiosResponse<CohereResponse>) => {
+        const generatedText = response.data.generations[0].text;
+        console.log(generatedText);
+      })
+      .catch((error: any) => {
+        console.error(error);
+      });
+  }
+}
---
+++ cohere-ai/test.ts
@@ -0,0 +1,4 @@
+import { Cohere } from "./index";
+
+const cohere = new Cohere("ntXqcgfwqzSMSHzspSYwpQrhJFg7LBawSzdyQWt9");
+cohere.fetchData();
